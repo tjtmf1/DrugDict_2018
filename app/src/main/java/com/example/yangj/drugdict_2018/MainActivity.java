672cc79eb24aa4037b41960d38633d0e7e84cbfd
yangja057 <yangja057@gmail.com>
2018-06-01 22:19:04 +0900

    MainActivity에 모든 Activity Button  생성

--- app/src/main/java/com/example/yangj/drugdict_2018/MainActivity.java
+++ app/src/main/java/com/example/yangj/drugdict_2018/MainActivity.java
@@ -1,7 +1,9 @@
 package com.example.yangj.drugdict_2018;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,40 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void onAddDrug(View view) {
+        Intent intent = new Intent(this, AddDrugActivity.class);
+        startActivity(intent);
+    }
+
+    public void onAddPrescription(View view) {
+        Intent intent = new Intent(this, AddPrescriptionActivity.class);
+        startActivity(intent);
+    }
+
+    public void onIntro(View view) {
+        Intent intent = new Intent(this, IntroActivity.class);
+        startActivity(intent);
+    }
+
+    public void onLogin(View view) {
+        Intent intent = new Intent(this, LoginActivity.class);
+        startActivity(intent);
+    }
+
+    public void onMyTaking(View view) {
+        Intent intent = new Intent(this, MyTakingActivity.class);
+        startActivity(intent);
+    }
+
+    public void onSearchDrug(View view) {
+        Intent intent = new Intent(this, SearchDrugActivity.class);
+        startActivity(intent);
+    }
+
+    public void onSignUp(View view) {
+        Intent intent = new Intent(this, SignUpActivity.class);
+        startActivity(intent);
     }
 }
