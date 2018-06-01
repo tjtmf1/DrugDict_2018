27bc89abfe7f6107a3609431a631b5c0815c6d49
yangja057 <yangja057@gmail.com>
2018-06-01 22:07:16 +0900

    내 복용약 Layout 수정 - FloatingActionButton 추가, Icon 추가

--- app/src/main/java/com/example/yangj/drugdict_2018/MyTakingActivity.java
+++ app/src/main/java/com/example/yangj/drugdict_2018/MyTakingActivity.java
@@ -1,13 +1,27 @@
 package com.example.yangj.drugdict_2018;
 
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MyTakingActivity extends AppCompatActivity {
+
+    FloatingActionButton fab;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_my_taking);
+
+        fab = findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Here's a Snackbar", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
     }
 }
