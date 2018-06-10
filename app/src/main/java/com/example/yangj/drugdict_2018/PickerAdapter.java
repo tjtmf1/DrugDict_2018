47e0f928be99cf0ac7c99a2bf3b7480a968dbb9e
Hyunwoo <tjtmf1@gmail.com>
2018-06-10 19:40:47 +0900

    SearchDrugActivity Horizontal picker 수정중

---
+++ app/src/main/java/com/example/yangj/drugdict_2018/PickerAdapter.java
@@ -0,0 +1,72 @@
+package com.example.yangj.drugdict_2018;
+
+import android.content.Context;
+import android.media.Image;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
+
+import java.util.List;
+
+public class PickerAdapter extends RecyclerView.Adapter<PickerAdapter.ColorPicker> {
+    private Context context;
+    private List<String> dataList;
+    private RecyclerView recyclerView;
+
+    public PickerAdapter(Context context, List<String> dataList, RecyclerView recyclerView) {
+        this.context = context;
+        this.dataList = dataList;
+        this.recyclerView = recyclerView;
+    }
+
+    @Override
+    public ColorPicker onCreateViewHolder(ViewGroup parent, int viewType) {
+        View view;
+        LayoutInflater inflater = LayoutInflater.from(context);
+        view = inflater.inflate(R.layout.picker_item_layout, parent, false);
+        return new PickerAdapter.ColorPicker(view);
+    }
+
+    @Override
+    public void onBindViewHolder(ColorPicker holder, final int position) {
+        ColorPicker colorPicker = holder;
+        colorPicker.textView.setText(dataList.get(position));
+        
+        colorPicker.layout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (recyclerView != null) {
+                    recyclerView.smoothScrollToPosition(position);
+                }
+            }
+        });
+    }
+
+    @Override
+    public int getItemCount() {
+        return dataList.size();
+    }
+
+    public void swapData(List<String> newData) {
+        dataList = newData;
+        notifyDataSetChanged();
+    }
+
+    class ColorPicker extends RecyclerView.ViewHolder {
+        TextView textView;
+        ImageView imageView;
+        LinearLayout layout;
+        public ColorPicker(View itemView) {
+            super(itemView);
+            layout = (LinearLayout)itemView.findViewById(R.id.picker);
+            imageView = (ImageView)itemView.findViewById(R.id.picker_image);
+            textView  =(TextView)itemView.findViewById(R.id.picker_text);
+        }
+    }
+}
--- app/src/main/java/com/example/yangj/drugdict_2018/SearchByShapeFragment.java
+++ app/src/main/java/com/example/yangj/drugdict_2018/SearchByShapeFragment.java
@@ -4,6 +4,9 @@ package com.example.yangj.drugdict_2018;
 import android.app.Fragment;
 import android.content.Context;
 import android.os.Bundle;
+import android.support.v7.widget.LinearSnapHelper;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.SnapHelper;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -13,6 +16,9 @@ import com.example.force.infodb.ProductInfo;
 import com.wefika.horizontalpicker.HorizontalPicker;
 
 import java.util.ArrayList;
+import java.util.List;
+
+import travel.ithaka.android.horizontalpickerlib.PickerLayoutManager;
 
 
 /**
@@ -44,6 +50,32 @@ public class SearchByShapeFragment extends Fragment {
         divisionPicker.setValues(divisionLine);
         colorPicker.setSelectedItem(2);
         shapePicker.setSelectedItem(1);
+
+        RecyclerView recyclerView = (RecyclerView)view.findViewById(R.id.rv);
+        PickerLayoutManager pickerLayoutManager = new PickerLayoutManager(getActivity(), PickerLayoutManager.HORIZONTAL, false);
+        pickerLayoutManager.setChangeAlpha(true);
+        pickerLayoutManager.setScaleDownBy(0.5f);
+        pickerLayoutManager.setScaleDownDistance(0.5f);
+
+        SnapHelper snapHelper = new LinearSnapHelper();
+        snapHelper.attachToRecyclerView(recyclerView);
+
+        recyclerView.setLayoutManager(pickerLayoutManager);
+        List<String> data = new ArrayList<>();
+        data.add("1");
+        data.add("2");
+        data.add("3");
+        data.add("4");
+        data.add("5");
+        data.add("6");
+        data.add("7");
+        data.add("8");
+        data.add("9");
+        PickerAdapter adapter = new PickerAdapter(getContext(), data, recyclerView);
+        recyclerView.setAdapter(adapter);
+
+
+
 
         Button btn = (Button)view.findViewById(R.id.shapeSearchBtn);
         btn.setOnClickListener(new View.OnClickListener() {
